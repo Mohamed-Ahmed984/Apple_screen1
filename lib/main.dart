c084d29acd1351fc6ffab9763407051623e9b5ea
Jack234567890 <mohamedrocket984@gmail.com>
2026-01-31 05:03:10 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: const Icon(Icons.arrow_back_ios_new_rounded),
+          actions: const [Icon(Icons.ios_share_rounded)],
+        ),
+        body: Column(
+          children: [
+            Image.network(
+              "https://image2url.com/r2/default/images/1769823426870-e2908525-e9a9-4ac9-b69a-74ac9786f333.webp",
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Naturel Red Apple",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          height: 1.2,
+                        ),
+                      ),
+                      Icon(Icons.favorite_border_rounded),
+                    ],
+                  ),
+                  const SizedBox(height: 6),
+                  const Text(
+                    "1kg, price \$4.99",
+                    style: TextStyle(fontSize: 16, height: 1.3),
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Row(
+                        children: [
+                          Icon(Icons.remove),
+                          SizedBox(width: 8),
+                          Text(
+                            "1",
+                            style: TextStyle(fontSize: 16, height: 1.3),
+                          ),
+                          SizedBox(width: 8),
+                          Icon(Icons.add, color: Colors.green),
+                        ],
+                      ),
+                      Text(
+                        "\$4.99",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          height: 1.25,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+              ),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Product details",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      height: 1.25,
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    "Apples are nutritious. Apples may be good for weight loss. Apples may be good for your heart as part of a healthy and balanced diet.",
+                    style: TextStyle(fontSize: 14, height: 1.3),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Nutritions",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      height: 1.3,
+                    ),
+                  ),
+                  Icon(Icons.arrow_forward_ios, size: 16),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Review",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      height: 1.3,
+                    ),
+                  ),
+                  Row(
+                    children: const [
+                      Icon(Icons.star, color: Colors.orange, size: 20),
+                      Icon(Icons.star, color: Colors.orange, size: 20),
+                      Icon(Icons.star, color: Colors.orange, size: 20),
+                      Icon(Icons.star, color: Colors.orange, size: 20),
+                      Icon(Icons.star, color: Colors.orange, size: 20),
+                      SizedBox(width: 4),
+                      Icon(Icons.arrow_forward_ios, size: 16),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
